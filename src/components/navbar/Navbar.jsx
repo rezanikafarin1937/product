06c68530a63682d34cbf9a1e903a24ebe2657e3f
rezanikafarin1937 <rezanikafarin700@gmail.com>
2026-08-22 14:50:32 -0700

    update Navbar for marking select item

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { routes } from "../../routes/RouteManager";
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
+  const location = useLocation();
+  const path = location.pathname;
+  console.log("path = ",path)
   return (
     <nav className={styles.nav}>
-        {routes.map((route,index) => (
-            <div>
-                <Link to={route.path} key={index}>{route.title}</Link>
-                <span style={{margin : "0 1rem"}}></span>
-            </div>
-        ))}
+      {routes.map((route, index) => (
+        <div  key={index}>
+            {console.log(" === ",route.path === path)}
+          <Link to={route.path} className={path  === route.path ? styles.nav__active : ""}>
+            {route.title}
+          </Link>
+          <span style={{ margin: "0 1rem" }}></span>
+        </div>
+      ))}
     </nav>
   );
 }
