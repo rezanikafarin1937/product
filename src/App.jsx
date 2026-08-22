a6844d7ed1ea44420c3becd9394f41066e75a855
rezanikafarin1937 <rezanikafarin700@gmail.com>
2026-08-22 08:09:12 -0700

    create src/pages/about and src/pages/products

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import About from "./pages/about/About";
 const App = () => {
   return (
     <Layout>
-      <Home />
+      <div className="wrapper">
+        <Home />
+        <Products />
+        <About />
+      </div>
     </Layout>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='wrapper'>
+    <div>
       <h1>Home</h1>
     </div>
   )
