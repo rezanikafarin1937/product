dc48061f221b6c91e719f212143b9fe3ec0bc119
rezanikafarin1937 <rezanikafarin700@gmail.com>
2026-09-07 22:00:00 -0700

    update for UserContext for isUserLogin

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext,useState } from "react";
+import Cookie from "js-cookie";
 
 const UserContext = createContext({});
 
@@ -7,7 +8,15 @@ export const useUserContext = () => {
 };
 
 export function UserProvider({ children }){
-    const [isUserLogin,setIsUserLogin] = useState(false)
+    const [isUserLogin,setIsUserLogin] = useState(()=>{
+        const token = Cookie.get("token");
+        if(token){
+            return true;
+        }
+        else{
+           return false;
+        }
+    })
     const handleLogin = () =>{
         setIsUserLogin(true)
     }
